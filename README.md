114a1d6d51093fcbf6f0d562d7bb0bb63dd51afd
BorArt22 <borart22@gmail.com>
2022-08-16 16:21:36 +0300

    song_data process is made

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# project-4-data-lake
-Fourth project in Udacity nanodegree program in Data Engineer for AI Applications. In this project creates a data lake for music streaming app in Amazon Cloud
+# Sparkify song play logs: Datalake
+This document describes how to create and load data to a datalake for analysing songs and user activity on Sparkify (music streaming app).
+
+Original datasets reside in S3 (Amazon Simple Storage Service in Amazon Web Services - AWS), in a directory of JSON logs on user activity on the app, as well as a directory with JSON metadata on the songs in the app.
+
+The datalake has been created in S3 (using EMR cluster in cloud-computing platform AWS) using a star schema. ETL pipeline that transfers the original  datasets into parquet files from json has been implemented using Spark. 
+
+# Table of Contents
+1. [Project Description](#project-description)
+2. [Original datasets](#original-datasets)
+3. [Project Files](#project-files)
+4. [Project Launching](#project-launching)
+5. [Process Initial Data](#process-initial-data)
+6. [Example queries and results for song play analysis](#example-queries-and-results-for-song-play-analysis)
+
+# Project Description
+A music streaming startup, Sparkify, has grown their user base and song database more and want to move their data warehouse to a data lake. Their data resides in S3, in a directory of JSON logs on user activity on the app, as well as a directory with JSON metadata on the songs in their app.
+
+In this way, a Datalake with tables designed to optimize queries on song play analysis has been created and the original datasets have been loaded into it.
+# Original datasets
+Datasets with original datasets with JSON metadata of the songs and  JSON logs of user activity on the app reside in S3. Here are the S3 links for each:
+
+-   Song data:  `s3://udacity-dend/song_data`
+-   Log data:  `s3://udacity-dend/log_data`
+
+Log data json path:  `s3://udacity-dend/log_json_path.json`
+
+## Song Dataset
+The first dataset is a subset of real data from the  [Million Song Dataset](http://millionsongdataset.com/). Each file is in JSON format and contains metadata about a song and the artist of that song. The files are partitioned by the first three letters of each song's track ID. For example, here are file paths to two files in this dataset.
+
+```
+song_data/A/B/C/TRABCEI128F424C983.json
+song_data/A/A/B/TRAABJL12903CDCF1A.json
+```
+
+And below is an example of what a single song file, TRAABJL12903CDCF1A.json, looks like:
+
+```
+{"num_songs": 1, "artist_id": "ARJIE2Y1187B994AB7", "artist_latitude": null, "artist_longitude": null, "artist_location": "", "artist_name": "Line Renaud", "song_id": "SOUPIRU12A6D4FA1E1", "title": "Der Kleine Dompfaff", "duration": 152.92036, "year": 0}
+```
+## Log Dataset
+The second dataset consists of log files in JSON format.
+
+The log files in the dataset are partitioned by year and month. For example, here are filepaths to two files in this dataset.
+
+```
+log_data/2018/11/2018-11-12-events.json
+log_data/2018/11/2018-11-13-events.json
+
+```
+
+And below is an example of what the data in a log file, 2018-11-12-events.json, looks like:
+
+![](https://video.udacity-data.com/topher/2019/February/5c6c15e9_log-data/log-data.png)
+
+# Project Files
+The project workspace includes four files:
+
+ -  `test.ipynb` displays the first few rows of each table to let check database and run queries from example.
+ -  `etl.py`  loads data from S3, transforms and save to S3 in parquet files.
+ -  `dl.cfg-example.properties`  example of config file for running `etl.py`.
+ -  `README.md`  provides discussion on the project.
+ 
+# Running in IPython
+**Load data**
+Run  `etl.py`  to read and process files from  `song_data`  and  `log_data`  and write data to parquet files in S3.
+```
+run etl.py
+```
+
+# Process initial data
+## Process song data
+
+## Process log data
+
+# Example queries and results for song play analysis
+### Query 1: Find all the users that has paid account and listen more than 10 songs, who are they
+
+
+### Query 2:  Most popular artist among paid users 
+
+
+### Query 3: How much did User 2 listen songs
--- etl.py
+++ etl.py
@@ -5,6 +5,8 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, col
 from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
 
+from pyspark.sql.window import Window
+from pyspark.sql.functions import row_number, asc, length
 
 config = configparser.ConfigParser()
 config.read('dl.cfg')
@@ -14,6 +16,9 @@ os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
 
 
 def create_spark_session():
+    '''
+    create spark session
+    '''    
     spark = SparkSession \
         .builder \
         .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
@@ -22,40 +27,59 @@ def create_spark_session():
 
 
 def process_song_data(spark, input_data, output_data):
-    # get filepath to song data file
-    song_data = 
-    
-    # read song data file
-    df = 
+    '''
+    process song data
+    - read song data from S3
+    - extract columns and delete duplicate in song_id to create songs table
+    - write songs table to parquet files partitioned by year and artist in S3
+    - extract columns and delete duplicate in artist_id to create artists table
+    - write artists table to parquet files in S3
+    '''    
 
-    # extract columns to create songs table
-    songs_table = 
+    song_data = os.path.join(input_data,'song_data/A/A/*/*.json')
     
-    # write songs table to parquet files partitioned by year and artist
-    songs_table
+    df = spark.read.json(song_data)
 
-    # extract columns to create artists table
-    artists_table = 
+    windowSpec_song  = Window.partitionBy("song_id").orderBy(asc("length_title"),asc("title"))
+    songs_table_prepared = df.select('song_id', 'title', 'artist_id', 'year', 'duration') \
+                             .withColumn("length_title",length("title")) \
+                             .withColumn("rn", row_number().over(windowSpec_song))
+    songs_table = songs_table_prepared.filter(songs_table_prepared.rn == 1) \
+                                      .select('song_id', 'title', 'artist_id', 'year', 'duration')
+
+    songs_table.write.partitionBy("year","artist_id").mode('overwrite').\
+                      parquet(output_data + "songs/songs_table.parquet")
+
+    windowSpec_artist  = Window.partitionBy("artist_id").orderBy(asc("artist_name"),asc("artist_name"))
+    artists_table_prepared = df.select('artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude') \
+                               .withColumn("length_artist_name",length("artist_name")) \
+                               .withColumn("rn", row_number().over(windowSpec_artist))
+    artists_table = artists_table_prepared.filter(artists_table_prepared.rn == 1) \
+                                          .select('artist_id', col('artist_name').alias('name'), col('artist_location').alias('location'), col('artist_latitude').alias('latitude'), col('artist_longitude').alias('longitude'))
     
-    # write artists table to parquet files
-    artists_table
+    artists_table.write.mode('overwrite').\
+                        parquet(output_data + "artists/artists_table.parquet")
 
 
 def process_log_data(spark, input_data, output_data):
+    '''
+    process log data
+    - read log data from S3
+    '''    
     # get filepath to log data file
-    log_data =
+    log_data = os.path.join(input_data,'log_data')
 
     # read log data file
-    df = 
+    df = spark.read.json(log_data)
     
     # filter by actions for song plays
     df = 
 
     # extract columns for users table    
-    artists_table = 
+    users_table = 
     
     # write users table to parquet files
-    artists_table
+    users_table
 
     # create timestamp column from original timestamp column
     get_timestamp = udf()
@@ -82,9 +106,14 @@ def process_log_data(spark, input_data, output_data):
 
 
 def main():
+    '''
+    - create spark session connecting to EMR cluster and using config data from dl.cfg file
+    - process song data
+    - process log data
+    '''
     spark = create_spark_session()
     input_data = "s3a://udacity-dend/"
-    output_data = ""
+    output_data = "s3://udacity-datalake-sparkify-bav/"
     
     process_song_data(spark, input_data, output_data)    
     process_log_data(spark, input_data, output_data)
